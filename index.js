04eb0b429e8965c13c1472ee28e46db4695ca44f
SofiaKononovych <105751740+SofiaKononovych@users.noreply.github.com>
2022-05-19 21:55:22 +0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,5 @@
+let number1 = prompt('Enter first number:', '');
+let number2 = prompt('Enter second number:', '');
+let a = Number(number1);
+let b = Number(number2);
+alert(`The sum is: ${a + b}`);
